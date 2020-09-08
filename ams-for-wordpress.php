222b41280d1f1f47b8e561e9d8f4c526422c455a
jaybhagwan <jay.bagwansingh@ifuturz.com>
2020-09-08 19:30:43 +0530

    ams setting

--- ams-for-wordpress.php
+++ ams-for-wordpress.php
@@ -275,12 +275,11 @@ function wpams_url_label_field_cb(){
 function wpams_apikey_label_field_cb(){ 
     // get the value of the setting we've registered with register_setting()
     $apikey = get_option('wpams_apikey_btn_label');
-    // output the field
     if(!empty($apikey))
     {
         $setting = sanitize_text_field("**************************************************************");
     }
-    
+
     ?>
     <input type="text" name="wpams_apikey_btn_label" style="width: 500px;" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
 

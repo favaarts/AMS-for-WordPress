fc6092e59d3cf722cc318ca99bc20f40517ba164
amscam <49993898+amscam@users.noreply.github.com>
2020-11-18 17:51:18 -0700

    Update eventlisting.php

--- inc/eventlisting.php
+++ inc/eventlisting.php
@@ -96,7 +96,7 @@ else
             <div class="evtlocation">
               <h4>Location</h4>
               <select class='ul-cat-wrap' id='evtlocation'>
-                <option value="">Locations</option>
+                <option value="">All Locations</option>
                 <?php
                 foreach($locationArrayResult['json']['locations'] as $c => $c_value) {
                   echo "<option  value='".$c_value."'>".$c_value."</option>";     
